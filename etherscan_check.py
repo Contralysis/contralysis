3717ff6d074e781dba19f9a4a3046445f337eba6
Sanghoon <76500799+DoBest20@users.noreply.github.com>
2024-06-06 23:23:04 +0900

    debug etherscan_check.py

--- etherscan_check.py
+++ etherscan_check.py
@@ -59,7 +59,7 @@ def check_address(address):
             return get_bytecode(address)
 
 print(check_address('0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5'))
-if API_KEY != None:
+if API_KEY != None and API_KEY != "":
     print("there are api key\n")
     print(API_KEY)
     
