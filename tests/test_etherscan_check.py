db859c0c30f45fd2bbebfce23207acc64ab4e610
Sanghoon <76500799+DoBest20@users.noreply.github.com>
2024-06-06 20:41:38 +0900

    Update test_etherscan_check.py

--- tests/test_etherscan_check.py
+++ tests/test_etherscan_check.py
@@ -1,5 +1,5 @@
 from etherscan_check import check_address
-
+import time
 
 class TestEtherscanCheck:
 
@@ -12,10 +12,12 @@ class TestEtherscanCheck:
             result_type = result[0]
             result_src = "".join(result[1].split())
             assert (result_type, result_src) == ('contract', src_code)
+            time.sleep(1)
 
 
     def test_wallet(self):
         assert check_address('0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5') == ('wallet', None)
-
+        time.sleep(1)
     def test_error(self):
         assert check_address('0xbb9bc244d798123fde783fcc1c72d3b223b8c183339414') == ('error', None)
+        time.sleep(1)
