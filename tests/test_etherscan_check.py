0f445637fd3d47aac814555c3cec90e5532f5876
Sanghoon <76500799+DoBest20@users.noreply.github.com>
2024-06-06 21:16:38 +0900

    Update test_etherscan_check.py

--- tests/test_etherscan_check.py
+++ tests/test_etherscan_check.py
@@ -2,7 +2,7 @@ from etherscan_check import check_address
 import time
 
 class TestEtherscanCheck:
-
+    @pytest.mark.order1
     def test_contract(self):
         src_dir = 'tests/test_etherscan_check'
         with open(f"{src_dir}/0x9bcCB0Dd17c1B2A62B70Ac4Bfad033a90CbA6F50.txt", "r", newline="\r\n") as f:
@@ -12,16 +12,12 @@ class TestEtherscanCheck:
             result_type = result[0]
             result_src = "".join(result[1].split())
             assert (result_type, result_src) == ('contract', src_code)
-            time.sleep(1)
-            assert check_address('0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5') == ('wallet', None)
-            time.sleep(1)
-            assert check_address('0xbb9bc244d798123fde783fcc1c72d3b223b8c183339414') == ('error', None)
-            
-
-    # def test_wallet(self):
-    #     time.sleep(1)
-    #     assert check_address('0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5') == ('wallet', None)
-    # def test_error(self):
-    #     time.sleep(2)
-    #     assert check_address('0xbb9bc244d798123fde783fcc1c72d3b223b8c183339414') == ('error', None)
+    @pytest.mark.order2   
+    def test_wallet(self):
+        time.sleep(1)
+        assert check_address('0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5') == ('wallet', None)
+    @pytest.mark.order3
+    def test_error(self):
+        time.sleep(2)
+        assert check_address('0xbb9bc244d798123fde783fcc1c72d3b223b8c183339414') == ('error', None)
 
